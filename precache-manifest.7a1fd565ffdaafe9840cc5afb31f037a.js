c756cd13be5b77dbe14d255e7b56bdae892a0a05
Asad Khilji <khiljiasad2@gmail.com>
2022-07-16 19:24:26 -0400

    Updates

---
+++ precache-manifest.7a1fd565ffdaafe9840cc5afb31f037a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8dadd6a7922a12e3af93affdea6a74",
+    "url": "/professional-portfolio/index.html"
+  },
+  {
+    "revision": "99b3821900681474d9fe",
+    "url": "/professional-portfolio/static/css/2.7b4682b0.chunk.css"
+  },
+  {
+    "revision": "e75d284cf0580502f825",
+    "url": "/professional-portfolio/static/css/main.4ca49420.chunk.css"
+  },
+  {
+    "revision": "99b3821900681474d9fe",
+    "url": "/professional-portfolio/static/js/2.c5fad6c8.chunk.js"
+  },
+  {
+    "revision": "f777c2cd8d43a44e423cc24a2b4ae71a",
+    "url": "/professional-portfolio/static/js/2.c5fad6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75d284cf0580502f825",
+    "url": "/professional-portfolio/static/js/main.ca31ed42.chunk.js"
+  },
+  {
+    "revision": "fd66dbdd2cb3a0283250",
+    "url": "/professional-portfolio/static/js/runtime-main.7f4f3631.js"
+  },
+  {
+    "revision": "e05eeb5a20f9025e1b8699ef5baac05c",
+    "url": "/professional-portfolio/static/media/avatar.e05eeb5a.png"
+  },
+  {
+    "revision": "4bb62f3c2a6c2b09fc75d23eaa74fcfb",
+    "url": "/professional-portfolio/static/media/bootstrap-logo.4bb62f3c.png"
+  },
+  {
+    "revision": "9bd652eec201a6792e7c2f383e2475c8",
+    "url": "/professional-portfolio/static/media/codingquiz.9bd652ee.png"
+  },
+  {
+    "revision": "aede7638c7f294e9d55643a0fc7b8026",
+    "url": "/professional-portfolio/static/media/faith.aede7638.png"
+  },
+  {
+    "revision": "6376322bb54aa542832449e14a60529d",
+    "url": "/professional-portfolio/static/media/flipbook.6376322b.png"
+  },
+  {
+    "revision": "b2a4d4c333f05d2aa226644e10be1ff0",
+    "url": "/professional-portfolio/static/media/frontend.b2a4d4c3.png"
+  },
+  {
+    "revision": "2ba25cef289dae768f21ef01511f99cc",
+    "url": "/professional-portfolio/static/media/full-stack.2ba25cef.png"
+  },
+  {
+    "revision": "1ce6c1d2ca3ac12c0ca15cb7085a15a7",
+    "url": "/professional-portfolio/static/media/horiseon.1ce6c1d2.png"
+  },
+  {
+    "revision": "437a92435cd15e95301021f96b07ef09",
+    "url": "/professional-portfolio/static/media/jesus.437a9243.png"
+  },
+  {
+    "revision": "379fe880d7638d8d5e6493d9dcfac94c",
+    "url": "/professional-portfolio/static/media/jquery-logo.379fe880.png"
+  },
+  {
+    "revision": "86695d5d2f75c08615e5db9c648e099f",
+    "url": "/professional-portfolio/static/media/logo.86695d5d.png"
+  },
+  {
+    "revision": "8967ca67bbedb067ba0aedd74a407295",
+    "url": "/professional-portfolio/static/media/mern-logo.8967ca67.png"
+  },
+  {
+    "revision": "4d9e6c123796f575472a7dae5fd7465f",
+    "url": "/professional-portfolio/static/media/mern.4d9e6c12.png"
+  },
+  {
+    "revision": "541dcec40560667c49cccd764392f3d4",
+    "url": "/professional-portfolio/static/media/mongodb-logo.541dcec4.png"
+  },
+  {
+    "revision": "be10ce756d73f87aa460b76049caf495",
+    "url": "/professional-portfolio/static/media/notetaker.be10ce75.png"
+  },
+  {
+    "revision": "f53bcd5a21878664469a00bf8d987316",
+    "url": "/professional-portfolio/static/media/password.f53bcd5a.png"
+  },
+  {
+    "revision": "bfbd07bb70d514b19b895b5273f74ec0",
+    "url": "/professional-portfolio/static/media/portfolio.bfbd07bb.png"
+  },
+  {
+    "revision": "19080b3cc1962fff3d289ffd360e640d",
+    "url": "/professional-portfolio/static/media/redux-logo.19080b3c.png"
+  },
+  {
+    "revision": "a90a28d226825fd53884a15c7572ec14",
+    "url": "/professional-portfolio/static/media/runbuddy.a90a28d2.png"
+  },
+  {
+    "revision": "21812f6d8d4844da17453b80216a4445",
+    "url": "/professional-portfolio/static/media/sql-logo.21812f6d.png"
+  },
+  {
+    "revision": "ae4f310c2ee9212c81a1b696521c0c84",
+    "url": "/professional-portfolio/static/media/tech-blog.ae4f310c.png"
+  },
+  {
+    "revision": "08f0c81fc056ac68a91d127aa3a13657",
+    "url": "/professional-portfolio/static/media/vuejs-logo.08f0c81f.png"
+  },
+  {
+    "revision": "c3f0accde03fd93cfd7bdfae9f2d757d",
+    "url": "/professional-portfolio/static/media/weather.c3f0accd.png"
+  },
+  {
+    "revision": "f7099627bd7c464de1c0b40abf87ad86",
+    "url": "/professional-portfolio/static/media/webpack-logo.f7099627.png"
+  },
+  {
+    "revision": "60ea4ba1203bee2b37360d7f6157df52",
+    "url": "/professional-portfolio/static/media/workday.60ea4ba1.png"
+  }
+]);
