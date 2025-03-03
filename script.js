47008dae304f8d46d58692cb846ba964efe21acd
website-template <khiljiasad2@gmail.com>
2025-03-03 11:30:39 -0500

    added

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3, // Show 3 slides on large screens
+    spaceBetween: 20, // Reduce gaps
+    loop: true, // Enable infinite scroll
+    centeredSlides: false, // Prevents unnecessary blank spaces
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1024: { slidesPerView: 3 },
+        768: { slidesPerView: 2 },
+        480: { slidesPerView: 1 },
+    }
+});
+
