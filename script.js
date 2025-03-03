8af21165e36399f9f13e1cb55a052f19d7a04115
asad-khilji <100327772+asad-khilji@users.noreply.github.com>
2025-03-03 14:22:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var swiper = new Swiper(".swiper-container", {
     breakpoints: {
         1024: { slidesPerView: 3 },
         768: { slidesPerView: 2 },
-        500: { slidesPerView: 1 },
+        400: { slidesPerView: 1 },
     }
 });
 
